e245541a3d7b44dbfa09ef17d3cfedcaedc18b23
joe-white09 <joe_white09@yahoo.com>
2022-08-05 12:26:54 -0500

    updated signup function

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,17 +3,14 @@ const { User } = require('../models');
 const userData = [
     {
         username: 'Joe1',
-        email: 'joe@fake.com',
         password: 'pass123'
     },
     {
         username: 'Malcome1',
-        email: 'malcome@fake.com',
         password: 'pass123'
     },
     {
         username: 'Wesley1',
-        email: 'wesley@fake.com',
         password: 'pass123'
     }
 
