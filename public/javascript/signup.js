aa597569fe7569d7377377512dc65e82145c80fa
joe-white09 <joe_white09@yahoo.com>
2022-08-08 16:52:49 -0500

    testing for heroku signup

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -21,28 +21,4 @@ async function signupFormHandler(event) {
     }
 }
 
-async function loginFormHandler(event) {
-    event.preventDefault();
-
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-
-    if (username && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
-        }
-    }
-}
-
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
