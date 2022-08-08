524b2af46b8f6187ab6769782953a0c0521ac6a7
joe-white09 <joe_white09@yahoo.com>
2022-08-08 16:59:05 -0500

    added email to signup

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -50,6 +50,7 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
     User.create({
         username: req.body.username,
+        email: req.body.email,
         password: req.body.password
     })
     .then(dbUserData => {
--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -2,13 +2,15 @@ async function signupFormHandler(event) {
     event.preventDefault();
 
     const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    if (username && password) {
+    if (username && email && password) {
         const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
                 username,
+                email,
                 password
             }),
             headers: { 'Content-Type': 'application/json' }
